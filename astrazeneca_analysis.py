41345cced415efeea9e7003b7577b2e8ffa69818
AmitTripathi12 <142001859+AmitTripathi12@users.noreply.github.com>
2023-08-11 14:35:15 +0530

    Update astrazeneca_analysis.py
    Changed Data Address

--- astrazeneca_analysis.py
+++ astrazeneca_analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # Create a DataFrame
-astrazeneca_df = pd.read_csv(r"C:\McDermott\Data Science\AstraZeneca Project\astrazeneca_data.csv")
+astrazeneca_df = pd.read_csv(r"AstraZeneca/astrazeneca_data.csv")
 
 # Streamlit app
 st.title("Medicine Ingredients Analysis")
