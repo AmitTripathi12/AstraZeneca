96de9da57d71094ac8dce1ff3fb57d721cc2e41c
AmitTripathi12 <142001859+AmitTripathi12@users.noreply.github.com>
2023-08-11 15:07:58 +0530

    Update astrazeneca_analysis.py
    Extended functionality of the app

--- astrazeneca_analysis.py
+++ astrazeneca_analysis.py
@@ -6,15 +6,16 @@ import pandas as pd
 astrazeneca_df = pd.read_csv("astrazeneca_data.csv")
 
 # Streamlit app
-st.title("Medicine Ingredients Analysis")
+st.title("Medicine Ingredients Analysis per year per route")
 
 # Sidebar for user input
 selected_year = st.sidebar.selectbox("Select Year", astrazeneca_df['Year'].unique())
 selected_route = st.sidebar.selectbox("Select Route", astrazeneca_df['route'].unique())
 
 # Filter the data based on user input
-filtered_data = astrazeneca_df[(astrazeneca_df['Year'] == selected_year) & (astrazeneca_df['route'] == selected_route)]
-average_ingredients = filtered_data['Number_Ingredients'].mean()
+filtered_data_route = astrazeneca_df[(astrazeneca_df['Year'] == selected_year) & (astrazeneca_df['route'] == selected_route)]
+average_ingredients_route = filtered_data_route['Number_Ingredients'].mean()
 
-# Display results
-st.write(f"Average Ingredients in {selected_route} medicines for {selected_year}: {average_ingredients:.2f}")
+filtered_data_year = astrazeneca_df[astrazeneca_df['Year'] == selected_year]
+average_ingredients_year = filtered_data_year['Number_Ingredients'].mean()
+
